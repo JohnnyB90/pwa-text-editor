bf49d48b529ecb654d5acfb610738e1ce62024e7
Johnny <117334322+JohnnyB90@users.noreply.github.com>
2023-06-18 01:36:09 -0400

    Made changes to database.js to console log the content pushed or pulled from database

---
+++ README.md
@@ -0,0 +1,54 @@
+
+## Table of Contents
+- [Name of Author](#name-of-author)
+- [Project Name](#project-name)
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Testing](#testing)
+  - [Contributors](#contributors)
+  - [License](#license)
+  - [Support](#support)
+  - [Questions](#questions)
+
+# Name of Author
+
+Johnny Bonee
+
+# Project Name
+
+Jate Text Editor
+
+## Description
+
+A tool great for keeping notes, or making lists
+
+
+## Installation
+
+No installation is required, but it can be installed from a browser and used offline.
+
+## Usage
+
+Simply navigate to the website (https://jate-just-another-editor-0cbe0f3e2698.herokuapp.com/) and begin to use. To install it just click the install button in the top left.
+
+## Testing
+
+I did the testing by trying the features.
+
+## Contributors
+
+Addtional contributors: N/A
+
+## License
+
+No licenses are assigned to this project.
+
+## Support
+
+johnny.bonee@ultimaterestoration.com
+
+## Questions
+
+For questions or feedback, please feel free to reach out to me <a href="https://github.com/johnnyb90">here</a> on GitHub.
+  
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -4,29 +4,40 @@ const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+        console.log('JATE database already exists!');
         return;
       }
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      console.log('JATE database created!');
     },
   });
 
 export const putDb = async (content) => {
+  try {
   const db = await openDB('jate', 1);
   const transaction = db.transaction('jate', 'readwrite');
   const store = transaction.objectStore('jate');
   await store.add(content);
   await transaction.done;
+  console.log("Content added to database!!")
+} catch (error) {
+  console.error('Error adding to database', error);
+}
 };
 
 export const getDb = async () => {
-  const db = await openDB('jate', 1);
-  const transaction = db.transaction('jate', 'readonly');
-  const store = transaction.objectStore('jate');
-  const content = await store.getAll();
-  await transaction.done;
-  return content;
+  try {
+    const db = await openDB('jate', 1);
+    const transaction = db.transaction('jate', 'readonly');
+    const store = transaction.objectStore('jate');
+    const content = await store.getAll();
+    await transaction.done;
+    console.log("Content recovered from database!!");
+    return content;
+  } catch (error) {
+    console.error('Error retrieving from database:', error);
+  }
 };
+
 
 initdb();
