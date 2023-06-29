9ab910f1e79ed0240e946ea43014179dd0afc55c
Johnny <117334322+JohnnyB90@users.noreply.github.com>
2023-06-29 18:30:35 -0400

    Correcting error

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        filename: 'index.html'
+        title: 'JATE',
       }),
       new WebpackPwaManifest({
         name: 'Just Another Text Editor',
